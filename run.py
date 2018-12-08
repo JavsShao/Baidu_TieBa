1a214522530c3a8dc2eedb42969c4ef1317b6bd5
hamdi <javs_shao@163.com>
2018-12-08 14:49:50 +0800

    基本配置

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import re
 import urllib.request
 
+
+# 基本配置
+url = 'https://tieba.baidu.com/p/5968543034'
+
+
 class Splider(object):
     def getHtml(url):
         '''
@@ -11,6 +16,8 @@ class Splider(object):
         page = urllib.request.urlopen(url)
         html = page.read()
         return html
+
+
 
 
 if __name__ == '__main__':
