d8b62ee06ffe75acb5e91e75f4cd2db8c4823917
hamdi <javs_shao@163.com>
2018-12-08 16:22:42 +0800

    初始化

--- run.py
+++ run.py
@@ -3,18 +3,11 @@ import urllib
 from lxml import etree
 
 
-class Splider(object):
+class Spider:
     def __init__(self):
-        '''
-        初始化
-        '''
-        self.tieba_name = input('请输入要访问的贴吧:')
-        self.begin_page = input('请输入起始页：')
-        self.end_page = input('请输入终止页:')
+        self.tiebaName = input("请需要访问的贴吧：")
+        self.beginPage = int(input("请输入起始页："))
+        self.endPage = int(input("请输入终止页："))
 
-        self.url = 'https://tieba.baidu.com/f'
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1 Trident/5.0;"
-        }
-        # 图片编号
-        self.user_name = 1
+        self.url = 'http://tieba.baidu.com/f'
+        self.ua_header = {"User-Agent" : "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1 Trident/5.0;"}
